7744405eeac5ee638d8cbdf8454cb4cec5e54f6d
Wojciech Adam Koszek <wojciech@koszek.com>
2016-08-28 14:35:45 +0200

    Add seeds for 2 books. p69.

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
+Product.create!(
+	title: 'Programming Ruby 1.9',
+	description:
+		%{<p>
+			Ruby is the fastest growing language
+		</p>},
+	image_url: 'ruby.jpg',
+	price: 49.95
+)
+Product.create!(
+	title: 'Coffeescript',
+	description:
+		%{<p>Coffeescript is Javascript done right. It provides all of
+			Javascripts' functionality wrapper in a cleaner, more succinct
+			syntax. In the first book on this exciting new language,
+			CoffeeScript guru Trevor Burnham shows how to
+		},
+	image_url: 'cs.jpg',
+	price: 36.00
+)
+
