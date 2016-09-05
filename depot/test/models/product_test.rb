85a602791178005dc443bd35567f902201fe2319
Wojciech Adam Koszek <wojciech@koszek.com>
2016-09-04 21:25:37 -0700

    Add the new product test.

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
+def new_product(image_url)
+  Product.new(
+      title: "My Book title",
+      description: "yyy",
+      price: 1,
+      image_url: image_url
+  )
+end
+
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
   test "Product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -31,4 +38,16 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
   end
+
+  test "image url" do
+    ok = %w{fred.gif fred.jpg fred.png FRED.JPG FREQ.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{fred.doc fred.gif/more fred.gif.more}
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid? "#{name} shouldn't be valid"
+    end
+  end
+
 end
